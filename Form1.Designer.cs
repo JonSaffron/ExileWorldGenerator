d906a4f05b0994428f3cdac6b8114b7e40d2e253
Jon Saffron <github@saffron.me.uk>
2019-06-14 16:28:44 +0100

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ExileMappedBackground
+    {
+    partial class Form1
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+            }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        }
+    }
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExileMappedBackground
+    {
+    public partial class Form1 : Form
+        {
+        const float size = 1.0f / 256.0f;
+        private CalculateBackground mapper = new CalculateBackground();
+
+        public Form1()
+            {
+            InitializeComponent();
+            }
+
+        private void Form1_Shown(object sender, EventArgs e)
+            {
+            this.tableLayoutPanel1.SuspendLayout();
+
+            this.tableLayoutPanel1.ColumnCount = 256;
+            this.tableLayoutPanel1.RowCount = 256;
+
+            for (int i = 0; i < 256; i++)
+                {
+                if (this.tableLayoutPanel1.RowStyles.Count < i)
+                    {
+                    this.tableLayoutPanel1.RowStyles[i].SizeType = SizeType.Absolute;
+                    this.tableLayoutPanel1.RowStyles[i].Height = 15f;
+                    }
+                else 
+                    {
+                    this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 15f));
+                    }
+
+                if (this.tableLayoutPanel1.ColumnStyles.Count < i)
+                    {
+                    this.tableLayoutPanel1.ColumnStyles[i].SizeType = SizeType.Absolute;
+                    this.tableLayoutPanel1.ColumnStyles[i].Width = 30f;
+                    }
+                else
+                    {
+                    this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 30f));
+                    }
+                }
+
+            var hashOfPositions = new HashSet<int>();
+            int c = 0;
+            byte y = 0;
+            do
+                {
+                byte x = 0;
+                do
+                    {
+                    var position = mapper.GetBackground(x, y);
+                    if (position.HasValue)
+                        {
+                        c++;
+                        hashOfPositions.Add(position.Value);
+                        var squareValue = mapper.GetMapData(position.Value, x, out string log);
+                        AddLocation(x, y, squareValue, log);
+                        }
+
+                    x += 1;
+                    if (x == 0)
+                        break;
+                    } while (true);
+                
+                y += 1;
+                if (y == 0)
+                    break;
+                } while (true);
+
+            this.tableLayoutPanel1.ResumeLayout();
+
+            MessageBox.Show("total of mapped data: " + c + "\r\nTotal unique positions:" + hashOfPositions.Count);
+            }
+
+        private void AddLocation(byte x, byte y, byte squareValue, string log)
+            {
+            var label = new Label();
+            this.tableLayoutPanel1.Controls.Add(label, x, y);
+            label.Dock = DockStyle.Fill;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Text = string.Format("{0:x2}{1}", 
+                                        squareValue & 0x3f, 
+                                        (squareValue & 0xC0) == 0xC0 ? "+" :
+                                            (squareValue & 0x40) != 0 ? "|" : 
+                                                (squareValue & 0x80) != 0 ? "-" : 
+                                                    string.Empty);
+            label.Font = new Font("Arial Narrow", 7f);
+
+            var tooltip = new ToolTip();
+            var coords = $"({x:X2}, {y:X2}\r\n{log}";
+            tooltip.SetToolTip(label, coords);
+            tooltip.ShowAlways = true;
+            }
+        }
+    }
