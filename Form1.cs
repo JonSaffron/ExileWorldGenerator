b909f73fa1e4a8a6f7b277e0f09b3a0294ee9472
Jon Saffron <github@saffron.me.uk>
2019-06-26 16:36:00 +0100

    Use databinding

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,87 +21,103 @@ namespace ExileMappedBackground
 
         private void Form1_Shown(object sender, EventArgs e)
             {
+            this.dataGridView1.SuspendLayout();
+
             this.dataGridView1.ColumnCount = 256;
             this.dataGridView1.RowCount = 256;
 
             for (int i = 0; i < 256; i++)
                 {
-                this.dataGridView1.Rows[i].HeaderCell.Value = i.ToString("X");
-                this.dataGridView1.Columns[i].HeaderCell.Value = i.ToString("X");
+                var value = i.ToString("X2");
+                this.dataGridView1.Rows[i].HeaderCell.Value = value;
+                this.dataGridView1.Columns[i].HeaderCell.Value = value;
+                this.dataGridView1.Columns[i].DataPropertyName = value;
                 }
 
-            var hashOfPositions = new HashSet<int>();
-            int c = 0;
-            byte y = 0;
-            var tasks = new ConcurrentBag<Task>();
-            TaskScheduler uiContext = TaskScheduler.FromCurrentSynchronizationContext();
-            do
-                {
-                System.Diagnostics.Trace.WriteLine(y.ToString());
+            var hashOfPositions = new ConcurrentBag<int>();
 
-                byte x = 0;
-                do
-                    {
-                    var x1 = x;
-                    var y1 = y;
-                    var task = Task.Factory.StartNew(() =>
-                        {
-                        string log = string.Empty;
-                        Color? backgroundColour = null;
+            var dt = BuildDataTable();
 
-                        var mapResult = mapper.GetBackground(x1, y1);
-                        if (mapResult.IsMappedData)
-                            {
-                            c++;
-                            hashOfPositions.Add(mapResult.PositionInMappedData);
-                            log = $"Position: {mapResult.PositionInMappedData}";
-                            backgroundColour = Color.Cornsilk;
-                            }
+            Parallel.For(0, 0x10000, i => { NewMethod(i, hashOfPositions, dt); });
 
-                        var squareValue = mapper.GetMapData(mapResult.Result, x1, ref log);
-                        });
-                    var continuedTask = task.ContinueWith(() =>
-                        {
-                        AddLocation(x1, y1, squareValue, backgroundColour, log);
-                        }, uiContext);
-
-                    tasks.Add(continuedTask);
-
-                    x += 1;
-                    if (x == 0)
-                        break;
-                    } while (true);
-                
-                y += 1;
-                if (y == 0)
-                    break;
-                } while (true);
-
-            MessageBox.Show("total of mapped data: " + c + "\r\nTotal unique positions:" + hashOfPositions.Count);
+            this.dataGridView1.DataSource = dt;
+            this.dataGridView1.ResumeLayout();
+            MessageBox.Show("total of mapped data: " + hashOfPositions.Count + "\r\nTotal unique positions:" + hashOfPositions.Distinct().Count());
             }
 
+        private void NewMethod(int i, ConcurrentBag<int> hashOfPositions, DataTable dt)
+            {
+            byte y = (byte) (i >> 8);
+            byte x = (byte) (i & 0xFF);
 
+            if (x == 0)
+                System.Diagnostics.Trace.WriteLine(y.ToString());
 
+            string log = string.Empty;
+            Color? backgroundColour = null;
+
+            var mapResult = mapper.GetBackground(x, y);
+            if (mapResult.IsMappedData)
+                {
+                hashOfPositions.Add(mapResult.PositionInMappedData);
+                log = $"Position: {mapResult.PositionInMappedData}";
+                backgroundColour = Color.Cornsilk;
+                }
+
+            var squareValue = mapper.GetMapData(mapResult.Result, x, ref log);
+
+            if (dt.Rows.Count <= y)
+                System.Diagnostics.Debugger.Break();
+            if (dt.Columns.Count <= x)
+                System.Diagnostics.Debugger.Break();
+
+            var dataRow = dt.Rows[y];
+            var column = x.ToString("X2");
+            var text = $"{squareValue & 0x3f:x2}{((squareValue & 0xC0) == 0xC0 ? "+" : (squareValue & 0x40) != 0 ? "|" : (squareValue & 0x80) != 0 ? "-" : string.Empty)}";
+            lock (dt.Rows.SyncRoot)
+                {
+                dataRow[column] = text;
+                }
+            AddLocation(x, y, squareValue, backgroundColour, log);
+            }
+
+        private DataTable BuildDataTable()
+            {
+            var result = new DataTable();
+            for (int i = 0; i < 0x100; i++)
+                {
+                result.Columns.Add(i.ToString("X2"), typeof(string));
+                }
+            for (int i = 0; i < 0x100; i++)
+                {
+                var dr = result.NewRow();
+                result.Rows.Add(dr);
+                }
+            return result;
+            }
+
+        delegate void AddLocationDelegate(byte x, byte y, byte squareValue, Color? backgroundColour, string log);
         private void AddLocation(byte x, byte y, byte squareValue, Color? backgroundColour, string log)
             {
-            if (this.dataGridView1.InvokeRequired)
-                {
-                this.dataGridView1.Invoke(AddLocation, new[] {x, y, squareValue, backgroundColour, log});
-                return;
-                }
+            //if (this.dataGridView1.InvokeRequired)
+            //    {
+            //    AddLocationDelegate thisMethodDelegated = AddLocation;
+            //    object[] args = {x, y, squareValue, backgroundColour, log};
+            //    this.dataGridView1.Invoke(thisMethodDelegated, args);
+            //    return;
+            //    }
 
             var cell = this.dataGridView1.Rows[y].Cells[x];
 
-            this.dataGridView1.in
-            cell.Value = string.Format("{0:x2}{1}", 
-                                        squareValue & 0x3f, 
-                                        (squareValue & 0xC0) == 0xC0 ? "+" :
-                                            (squareValue & 0x40) != 0 ? "|" : 
-                                                (squareValue & 0x80) != 0 ? "-" : 
-                                                    string.Empty);
+            //cell.Value = string.Format("{0:x2}{1}", 
+            //                            squareValue & 0x3f, 
+            //                            (squareValue & 0xC0) == 0xC0 ? "+" :
+            //                                (squareValue & 0x40) != 0 ? "|" : 
+            //                                    (squareValue & 0x80) != 0 ? "-" : 
+            //                                        string.Empty);
             if (backgroundColour.HasValue)
                 cell.Style.BackColor = backgroundColour.Value;
-            if (squareValue < 0x10)
+            if ((squareValue & 0x3f) < 0x10)
                 cell.Style.ForeColor = Color.Red;
 
             var coords = $"({x:X2}, {y:X2})\r\n{log}";
